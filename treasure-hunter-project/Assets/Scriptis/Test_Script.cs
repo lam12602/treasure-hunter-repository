95f012da97d754f0f44146405fbfc2c1a9fd6687
lam12602 <79139004+lam12602@users.noreply.github.com>
2021-03-09 10:57:02 +0000

    Added test script to learn scripting basics

---
+++ treasure-hunter-project/Assets/Scriptis/Test_Script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Script : MonoBehaviour
+{
+
+    //This variable is editable in unity!
+    public string startMessage;
+    public string updateMessage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogError(startMessage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.LogWarning(updateMessage);
+    }
+}

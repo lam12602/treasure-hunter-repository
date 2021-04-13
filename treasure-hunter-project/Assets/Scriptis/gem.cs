c27752cefac4daf6fe083d8d13c3accf00f3c158
lam12602 <79139004+lam12602@users.noreply.github.com>
2021-04-13 10:53:34 +0100

    Tiles and scene transition added

---
+++ treasure-hunter-project/Assets/Scriptis/gem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class gem : MonoBehaviour
+{
+
+    public string levelToLoad;
+
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        Debug.Log("Object hit the gem!");
+
+        if (otherCollider.tag == "Player")
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+
+        
+    }
+        
+    }
+

09a034ccb052eded7496f13e71dcdae0db319bad
lam12602 <79139004+lam12602@users.noreply.github.com>
2021-04-20 09:38:40 +0100

    added the script that will kill the player when colliding with an enemy

---
+++ treasure-hunter-project/Assets/Scriptis/Hazard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collisionData)
+    {
+        //get the object collieded with
+        Collider2D objectWeColidedWith = collisionData.collider;
+ 
+        //get the PlayerHealth script if there is one attached
+        PlayerHealth player = objectWeColidedWith.GetComponent<PlayerHealth>();
+
+        if (player != null)
+        {
+            //this means there was a playerhealth script on the object
+            //which means the object is the player
+            player.Kill();
+
+        }
+    }
+}
---
+++ treasure-hunter-project/Assets/Scriptis/PlayerHealth.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    //this function is not bulit in and will be called by our own code
+    //it must be marked public so other scripts can use it
+    public void Kill()
+    {
+        //this will destroy the game object the script is attached to
+        Destroy(gameObject);
+    }
+}

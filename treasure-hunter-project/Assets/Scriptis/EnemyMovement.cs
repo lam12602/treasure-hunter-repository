92022dfa3e1397a291b45a8f489da2b2426562fb
lam12602 <79139004+lam12602@users.noreply.github.com>
2021-04-27 10:05:12 +0100

    added enemy movement

---
+++ treasure-hunter-project/Assets/Scriptis/EnemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    //public varibales
+    public float forceStrength; // how fast enemy moves
+    public Vector2 direction;
+    //private variables
+    private Rigidbody2D ourRigidBody;
+
+    //awake is called every time script is first loaded
+    void Awake()
+    {
+        ourRigidBody = GetComponent<Rigidbody2D>();
+        direction = direction.normalized;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ourRigidBody.AddForce(direction * forceStrength);
+    }
+}

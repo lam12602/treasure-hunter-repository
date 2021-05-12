f41ef5bff2f8f6ac80daba07407ef7553d498ef7
lam12602 <79139004+lam12602@users.noreply.github.com>
2021-05-12 09:08:46 +0100

    level 3 and coin sound added

--- treasure-hunter-project/Assets/Scriptis/ScorePickUp.cs
+++ treasure-hunter-project/Assets/Scriptis/ScorePickUp.cs
@@ -6,9 +6,11 @@ public class ScorePickUp : MonoBehaviour
 {
     //public variable
     public int pickupValue = 1;
+    public AudioClip coinSoud;
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        AudioSource.PlayClipAtPoint(coinSoud, transform.position);
         //Check if score script is attached to thing bumped into
         Score scoreScript = other.GetComponent<Score>();
         if (scoreScript !=null)
